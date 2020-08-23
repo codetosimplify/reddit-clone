eed5d7c8dad9ad212ccc90df00eddb226419ed6c
codetosimplify <codetosimplify@gmail.com>
2020-08-22 22:35:12 -0700

    initial apolloServer setup

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,2 +1,3 @@
-const PRODUCTION = "production";
+export const PRODUCTION = "production";
+export const PORT = 4000;
 export const isProduction = process.env.NODE_ENV === PRODUCTION;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,38 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import { PORT } from "./constants";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig); // connect db
   // do migration
+  //   await orm.getMigrator().up();
   const migrator = orm.getMigrator();
   await migrator.createMigration();
   //   await migrator.up();
+
+  const app = express();
+  // rest API way
+  // app.get("/", (_, res) => res.send("hello"));
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+    }),
+  });
+  apolloServer.applyMiddleware({ app });
+  app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
   // run sql
-  const post = orm.em.create(Post, { title: "first post" });
-  await orm.em.persistAndFlush(post);
-  //   console.log("------sql 2--------");
-  //   await orm.em.nativeInsert(Post, { title: "second post" });
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  //   const post = orm.em.create(Post, { title: "first post" });
+  //   await orm.em.persistAndFlush(post);
+  //   //   console.log("------sql 2--------");
+  //   //   await orm.em.nativeInsert(Post, { title: "second post" });
+  //   const posts = await orm.em.find(Post, {});
+  //   console.log(posts);
 };
 
 main().catch((err) => console.error(err));
