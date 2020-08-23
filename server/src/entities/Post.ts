4420e17832f98a26fd25212eb66f78ed1372cc59
codetosimplify <codetosimplify@gmail.com>
2020-08-23 07:50:40 -0700

    crud Post resolvers with mikro-orm

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => Date)
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => Date)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field() // exposes so that we can query from graphql
   @Property({ type: "text" })
   title!: string;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,17 +2,17 @@ import { MikroORM } from "@mikro-orm/core";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { Post } from "./entities/Post";
+
 import mikroConfig from "./mikro-orm.config";
 import { PORT } from "./constants";
-import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig); // connect db
   // do migration
   //   await orm.getMigrator().up();
-  const migrator = orm.getMigrator();
-  await migrator.createMigration();
+  // const migrator = orm.getMigrator();
+  // await migrator.createMigration();
   //   await migrator.up();
 
   const app = express();
@@ -20,8 +20,9 @@ const main = async () => {
   // app.get("/", (_, res) => res.send("hello"));
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [PostResolver],
     }),
+    context: () => ({ em: orm.em }),
   });
   apolloServer.applyMiddleware({ app });
   app.listen(PORT, () => console.log(`listening on ${PORT}`));
